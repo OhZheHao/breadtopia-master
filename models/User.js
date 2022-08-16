04a3e2778ec34b1c7d1cea67ba3a177d6af1328a
Chan Yi Da <yida.chan@yahoo.com>
2022-08-16 10:42:47 +0800

    a

--- models/User.js
+++ models/User.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 const db = require('../config/DBConfig');
 
-// Create users table in MySQL Database
+
 const User = db.define('user',
     {
         name: { type: Sequelize.STRING },
